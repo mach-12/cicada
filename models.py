282ed42986efb84756ef8654f6262eeb19979113
Mann Dharmesh Acharya <mann.compi@gmail.com>
2025-03-18 21:54:13 +0530

    Attempt to create HGQ version for the CicadaV1 and CicadaV2 models

--- models.py
+++ models.py
@@ -9,9 +9,12 @@ from tensorflow.keras.layers import (
     Input,
     Reshape,
     UpSampling2D,
-    Conv2DTranspose
+    Conv2DTranspose,
 )
-from qkeras import QActivation, QConv2D, QDense, QDenseBatchnorm, quantized_bits
+# from qkeras import QActivation, QConv2D, QDense, QDenseBatchnorm, quantized_bits
+from HGQ.layers import HActivation, HConv2D, HDenseBatchNorm, HConv2DBatchNorm, HDense
+
+from HGQ.quantizer import quantizer
 
 
 class TeacherAutoencoder:
@@ -66,7 +69,9 @@ class TeacherAutoencoderRevised:
         x = Activation("relu", name="teacher_relu_3")(x)
         x = Conv2D(30, (3, 3), strides=1, padding="same", name="teacher_conv2d_3")(x)
         x = Activation("relu", name="teacher_relu_4")(x)
-        x = Conv2DTranspose(30, (3, 3), strides=2, padding="same", name="teacher_conv_transpose")(x)
+        x = Conv2DTranspose(
+            30, (3, 3), strides=2, padding="same", name="teacher_conv_transpose"
+        )(x)
         x = Conv2D(20, (3, 3), strides=1, padding="same", name="teacher_conv2d_4")(x)
         x = Activation("relu", name="teacher_relu_5")(x)
         outputs = Conv2D(
@@ -80,62 +85,103 @@ class TeacherAutoencoderRevised:
         return Model(inputs, outputs, name="teacher-transpose")
 
 
-class CicadaV1:
-    def __init__(self, input_shape: tuple):
-        self.input_shape = input_shape
+# class CicadaV1:
+#     def __init__(self, input_shape: tuple):
+#         self.input_shape = input_shape
 
-    def get_model(self):
-        inputs = Input(shape=self.input_shape, name="inputs_")
-        x = QDenseBatchnorm(
-            16,
-            kernel_quantizer=quantized_bits(8, 1, 1, alpha=1.0),
-            bias_quantizer=quantized_bits(8, 3, 1, alpha=1.0),
-            name="dense1",
-        )(inputs)
-        x = QActivation("quantized_relu(10, 6)", name="relu1")(x)
-        x = Dropout(1 / 8)(x)
-        x = QDense(
-            1,
-            kernel_quantizer=quantized_bits(12, 3, 1, alpha=1.0),
-            use_bias=False,
-            name="dense2",
-        )(x)
-        outputs = QActivation("quantized_relu(16, 8)", name="outputs")(x)
-        return Model(inputs, outputs, name="cicada-v1")
+#     def get_model(self):
+#         inputs = Input(shape=self.input_shape, name="inputs_")
+#         x = QDenseBatchnorm(
+#             16,
+#             kernel_quantizer=quantized_bits(8, 1, 1, alpha=1.0),
+#             bias_quantizer=quantized_bits(8, 3, 1, alpha=1.0),
+#             name="dense1",
+#         )(inputs)
+#         x = QActivation("quantized_relu(10, 6)", name="relu1")(x)
+#         x = Dropout(1 / 8)(x)
+#         x = QDense(
+#             1,
+#             kernel_quantizer=quantized_bits(12, 3, 1, alpha=1.0),
+#             use_bias=False,
+#             name="dense2",
+#         )(x)
+#         outputs = QActivation("quantized_relu(16, 8)", name="outputs")(x)
+#         return Model(inputs, outputs, name="cicada-v1")
 
 
-class CicadaV2:
+# class CicadaV2:
+#     def __init__(self, input_shape: tuple):
+#         self.input_shape = input_shape
+
+#     def get_model(self):
+#         inputs = Input(shape=self.input_shape, name="inputs_")
+#         x = Reshape((18, 14, 1), name="reshape")(inputs)
+#         x = QConv2D(
+#             4,
+#             (2, 2),
+#             strides=2,
+#             padding="valid",
+#             use_bias=False,
+#             kernel_quantizer=quantized_bits(12, 3, 1, alpha=1.0),
+#             name="conv",
+#         )(x)
+#         x = QActivation("quantized_relu(10, 6)", name="relu0")(x)
+#         x = Flatten(name="flatten")(x)
+#         x = Dropout(1 / 9)(x)
+#         x = QDenseBatchnorm(
+#             16,
+#             kernel_quantizer=quantized_bits(8, 1, 1, alpha=1.0),
+#             bias_quantizer=quantized_bits(8, 3, 1, alpha=1.0),
+#             name="dense1",
+#         )(x)
+#         x = QActivation("quantized_relu(10, 6)", name="relu1")(x)
+#         x = Dropout(1 / 8)(x)
+#         x = QDense(
+#             1,
+#             kernel_quantizer=quantized_bits(12, 3, 1, alpha=1.0),
+#             use_bias=False,
+#             name="dense2",
+#         )(x)
+#         outputs = QActivation("quantized_relu(16, 8)", name="outputs")(x)
+#         return Model(inputs, outputs, name="cicada-v2")
+
+
+# class CicadaV1HGQ:
+#     def __init__(self, input_shape: tuple):
+#         self.input_shape = input_shape
+
+#     def get_model(self):
+#         inputs = Input(shape=self.input_shape, name="inputs_")
+#         x = HDenseBatchNorm(
+#             units=16,
+#         )(inputs)
+#         x = HActivation(activation="relu")(x)
+#         x = Dropout(1 / 8)(x)
+#         x = HDense(units=1, use_bias=False)(x)
+#         outputs = HActivation(activation="relu")(x)
+#         return Model(inputs, outputs, name="cicada-v1")
+
+
+class CicadaV2HGQ:
     def __init__(self, input_shape: tuple):
         self.input_shape = input_shape
 
     def get_model(self):
         inputs = Input(shape=self.input_shape, name="inputs_")
         x = Reshape((18, 14, 1), name="reshape")(inputs)
-        x = QConv2D(
-            4,
-            (2, 2),
+        x = HConv2D(
+            filters=4,
+            kernel_size=(2, 2),
             strides=2,
             padding="valid",
             use_bias=False,
-            kernel_quantizer=quantized_bits(12, 3, 1, alpha=1.0),
-            name="conv",
         )(x)
-        x = QActivation("quantized_relu(10, 6)", name="relu0")(x)
+        x = HActivation(activation="relu")(x)
         x = Flatten(name="flatten")(x)
         x = Dropout(1 / 9)(x)
-        x = QDenseBatchnorm(
-            16,
-            kernel_quantizer=quantized_bits(8, 1, 1, alpha=1.0),
-            bias_quantizer=quantized_bits(8, 3, 1, alpha=1.0),
-            name="dense1",
-        )(x)
-        x = QActivation("quantized_relu(10, 6)", name="relu1")(x)
+        x = HDenseBatchNorm(units=16)(x)
+        x = HActivation("relu")(x)
         x = Dropout(1 / 8)(x)
-        x = QDense(
-            1,
-            kernel_quantizer=quantized_bits(12, 3, 1, alpha=1.0),
-            use_bias=False,
-            name="dense2",
-        )(x)
-        outputs = QActivation("quantized_relu(16, 8)", name="outputs")(x)
+        x = HDense(units=1)(x)
+        outputs = HActivation(activation="relu")(x)
         return Model(inputs, outputs, name="cicada-v2")
